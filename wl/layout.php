51809950fff6b389b41d7b1d21867a0230b2d5c8
Igor Muravinets <muravinets@gmail.com>
2017-04-05 16:18:19 +0300

    WL

---
+++ wl/layout.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+	<meta charset="utf-8"/>
+	<title>Layout WL Поисковик авиабилетов</title>
+	
+	<link rel="stylesheet" type="text/css" href="http://fresh.aviafox.com/main.css?r=0.22104153468856458" />
+	<link rel="stylesheet" type="text/css" href="css/layout.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/style-tpwl.css" />
+	<link rel="stylesheet" type="text/css" href="css/head-style-id.css" />
+	<link rel="stylesheet" type="text/css" href="//www.travelpayouts.com/mewtwo_a/styles.css" />
+	<link rel="stylesheet" type="text/css" href="css/head-style.css" />
+	<link rel="stylesheet" type="text/css" href="css/form.css" />
+	
+</head>
+<body>
+
+<section class="bg-image">
+	<img src="//aviafox.com/web/images/img-05.jpg" />
+</section>
+
+<header>
+	<section class="top-bar">
+        <section class="logo">
+        	<img src="//aviafox.com/web/images/logo-320x154.png" alt="Afiafox logo" />
+        </section>	
+		<section class="subtitle">Поисковик авиабилетов</section>
+    </section>	
+</header>
+
+<div class="TPWL-widget" style="width: 100%; height: 300px; border: 0px solid red; background-color: silver;">
+</div>
+
+<footer>
+	<div class="footer-copyright">AviaFox © 2016</div>
+    <div class="footer-description">Поиск и бронирование дешёвых авиабилетов и отелей</div>    	
+</footer>
+
+</body>
+</html>
