31361ed8d1eacd0c15b6b5a60e2e222927caffa8
Igor Muravinets <muravinets@gmail.com>
2017-04-15 18:10:11 +0300

    Content indent disable

--- wp-content/themes/aviafox/template-parts/head/standart.php
+++ wp-content/themes/aviafox/template-parts/head/standart.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/css/header/styles.css?v=2.1" media="all" />
 
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/css/content-layout.css?v=3.1" media="all" />
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/css/content-styles.css?v=3.0" media="all" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory') ?>/assets/css/content-styles.css?v=3.1" media="all" />
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 
