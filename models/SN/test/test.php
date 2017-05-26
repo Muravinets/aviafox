722fd5a78e27f852d744b4aed80f4a95175d91db
Igor Muravinets <muravinets@gmail.com>
2017-05-26 21:38:58 +0300

    Social Networks meta

--- models/SN/test/test.php
+++ models/SN/test/test.php
@@ -1,17 +1,22 @@
-<!DOCTYPE html>
-<html lang="ru-RU">
+<?
+include 'config.php';
+/* @var $title string */
+/* @var $description string */
+
+?><!DOCTYPE html>
+<html lang="ru-RU" prefix="og: http://ogp.me/ns#">
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Test Title</title>
-	<meta name="description" content="Test Description" />
+	<title><?= $title ?></title>
+	<meta name="description" content="<?= $description ?>" />
 
 	<link href="/favicon.ico" rel="icon" type="image/x-icon" />
 	<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
 
-	<meta property="og:title" content="Web Meta Info - og:title"/>
-    <meta property="og:description" content="Проверьте, удовлетворяет ли ваш проект og:description"/>
+	<meta property="og:title" content="<?= $title ?>"/>
+    <meta property="og:description" content="<?= $description ?>"/>
 	<meta property="og:type" content="article"/>
 	<meta property="og:url" content="https://www.aviafox.com/models/SN/test/index.php"/>
 	<meta property="og:site_name" content="Aviafox.com"/>
