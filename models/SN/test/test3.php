db35c93f2fcd2284db6425975ae6578e12da1a58
Igor Muravinets <muravinets@gmail.com>
2017-05-26 14:55:04 +0300

    Social Networks meta

---
+++ models/SN/test/test3.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Test Title</title>
+	<meta name="description" content="Test Description" />
+
+	<link href="/favicon.ico" rel="icon" type="image/x-icon" />
+	<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+	<meta property="og:title" content="Test OG Title"/>
+	<meta property="og:description" content="Test OG Description"/>
+	<meta property="og:type" content="article"/>
+	<meta property="og:url" content="https://www.aviafox.com/models/SN/test/index.php"/>
+	<meta property="og:site_name" content="Aviafox.com"/>
+<!--	<meta property="fb:admins" content="USER_ID"/>-->
+
+	<meta property="og:image" content="https://www.aviafox.com/web/images/logo-rectangle.jpg"/>
+	<meta property="og:image" content="https://www.aviafox.com/web/images/logo-square.jpg"/>
+
+</head>
+<body>
+	<h1>Test Social Networks Content</h1>
+</body>
+</html>
