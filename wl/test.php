40f83227bac9681b78e2f03f2659c67c30c0cb15
Igor Muravinets <muravinets@gmail.com>
2018-01-21 23:08:01 +0300

    Sky Stars WL

--- wl/test.php
+++ wl/test.php
@@ -7,6 +7,16 @@
 
 </head>
 <body>
+
+<style type="text/css">
+.mewtwo-widget--whitelabel_ru .mewtwo-tabs-tabs_list__item--active:after {
+    background: none !important;
+}
+.mewtwo-widget--whitelabel_ru .mewtwo-flights .mewtwo-show_hotels-wrapper {
+background: none !important;
+}
+</style>
+
 	<script charset="utf-8" src="//www.travelpayouts.com/widgets/9d7a5dd68fa589e083125ce486b4cd17.js?v=1041" async></script>
 </body>
 </html>
