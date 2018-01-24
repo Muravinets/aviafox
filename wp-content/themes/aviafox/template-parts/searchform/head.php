ff8cfc4b9cfea8bf22aa11018735eb139fd9bc3f
Igor Muravinets <muravinets@gmail.com>
2018-01-24 18:40:56 +0300

    Sky Stars Search Form

--- wp-content/themes/aviafox/template-parts/searchform/head.php
+++ wp-content/themes/aviafox/template-parts/searchform/head.php
@@ -1,5 +1,5 @@
 <?
-$styleVersion = '?v=' . ($_SERVER['HTTP_HOST'] == 'wp.aviafox.local' ? time() : '5.31');
+$styleVersion = '?v=' . ($_SERVER['HTTP_HOST'] == 'wp.aviafox.local' ? time() : '5.32');
 $stylesBaseUri = get_bloginfo('template_directory', 'display') . '/assets/css/search-form';
 
 ?><!--SF css files-->
