f5ac3d80269d9d2291aa12c5d8811cb83808a439
Igor Muravinets <muravinets@gmail.com>
2019-05-13 21:01:55 +0300

    feat: Add VK tracking

--- models/Tracker.php
+++ models/Tracker.php
@@ -8,98 +8,107 @@
  */
 class Tracker
 {
+    /**
+     * Campaign's code
+     * @var string
+     */
+    private $code;
 
-	/**
-	 * Campaign's code
-	 * @var string
-	 */
-	private $code;
+    /**
+     * @var string
+     */
+    private $paramName = 'znc';
 
-	/**
-	 * @var string
-	 */
-	private $paramName = 'znc';
-
-	public function process()
-	{
-		if (isset($_GET[$this->paramName]))
-		{
-			$this->setCode(
-				$this->formatParam($_GET[$this->paramName])
-			);
-
-			setcookie($this->paramName, $this->getCode(), time()+60*60*24*30, '/');
-		}
-		else
-		{
-			if (!isset($_COOKIE[$this->paramName]))
-				return;
-
-			$this->setCode(
-				$this->formatParam($_COOKIE[$this->paramName])
-			);
-		}
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getUri()
-	{
-		return $this->paramName . '=' . $this->getCode();
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function hasCode()
-	{
-		return !is_null($this->code);
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getCode()
-	{
-		return $this->code;
-	}
-
-	/**
-	 * @param string $code
-	 */
-	private function setCode(string $code)
-	{
-		$this->code = $code;
-	}
-
-	/**
-	 * @param string $value
-	 *
-	 * @return string
-	 */
-	protected function formatParam($value)
+    public function process()
     {
-	    $value = trim($value);
-	    $value = stripslashes($value);
-	    $value = htmlspecialchars($value);
+        $newCode = null;
+        if ($this->detectVk()) {
+            $newCode = 'vk';
+        } elseif (isset($_GET[$this->paramName])) {
+            $newCode = $_GET[$this->paramName];
+        }
+
+        if (!is_null($newCode)) {
+            $this->setCode($this->formatParam($newCode));
+            $this->saveParamToCookies();
+
+        } elseif (isset($_COOKIE[$this->paramName])){
+            $newCode = $_COOKIE[$this->paramName];
+            $this->setCode($this->formatParam($newCode));
+        }
+    }
+
+    protected function detectVk(): bool
+    {
+        return
+            isset($_SERVER['HTTP_REFERER'])
+            && (stripos($_SERVER['HTTP_REFERER'], 'vk.com') !== false)
+        ;
+    }
+
+    protected function saveParamToCookies()
+    {
+        setcookie($this->paramName, $this->getCode(), time()+60*60*24*30, '/');
+    }
+
+    /**
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->paramName . '=' . $this->getCode();
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCode()
+    {
+        return !is_null($this->code);
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    private function setCode(string $code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    protected function formatParam($value)
+    {
+        $value = trim($value);
+        $value = stripslashes($value);
+        $value = htmlspecialchars($value);
         return $value;
     }
 
-	/**
-	 * @var self
-	 */
+    /**
+     * @var self
+     */
     protected static $instance;
 
-	/**
-	 * @return self
-	 */
-	public static function gi()
-	{
-		if (is_null(self::$instance))
-			self::$instance = new self;
+    /**
+     * @return self
+     */
+    public static function gi()
+    {
+        if (is_null(self::$instance))
+            self::$instance = new self;
 
-		return self::$instance;
-	}
-
+        return self::$instance;
+    }
 }
